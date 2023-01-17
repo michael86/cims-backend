d10313d07235c1ce66c0758ebcea973a8a5956b0
michael86 <michael8t6@gmail.com>
2023-01-17 18:59:37 +0000

    refactor update token into a util

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,18 @@
 const express = require("express");
-
-const { updateToken, update } = require("../mysql/query");
-const { runQuery } = require("../utils/sql");
-
+const { updateToken } = require("../utils/sql");
 const router = express.Router();
 
 //If user makes it here, they passed the auth middleware
 router.get("/", async function (req, res) {
   const { newToken: token, tokenId } = req.headers;
 
-  try {
-    const { affectedRows } = await runQuery(
-      update("tokens", [["token", `'${token}'`]], ["id", tokenId])
-    );
+  const update = await updateToken(
+    "tokens",
+    [["token", `'${token}'`]],
+    ["id", tokenId]
+  );
 
-    if (!affectedRows) {
-      res.status(500).send({ status: 0 });
-    }
-  } catch (err) {
-    console.log("auth error", err);
-    res.status({ status: 500 }).send({ status: 0 });
-  }
-
-  res.send({ status: 1, token });
+  update ? res.send({ status: 1, token }) : res.status(500).send({ status: 0 });
 });
 
 module.exports = router;
--- utils/sql.js
+++ utils/sql.js
@@ -1,4 +1,5 @@
 const asyncMySQL = require("../mysql/connection");
+const { update } = require("../mysql/query");
 
 module.exports.runQuery = async (query, data) => {
   try {
@@ -12,5 +13,24 @@ module.exports.runQuery = async (query, data) => {
     return res;
   } catch (err) {
     console.error(err);
+  }
+};
+
+module.exports.updateToken = async (location, values, target) => {
+  try {
+    const { affectedRows } = await this.runQuery(
+      update(location, values, target)
+    );
+
+    console.log("affected", affectedRows);
+
+    if (!affectedRows) {
+      return;
+    }
+
+    return true;
+  } catch (err) {
+    console.log("auth error", err);
+    res.status({ status: 500 }).send({ status: 0 });
   }
 };
